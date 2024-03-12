6e382fc5b64a0c2e2adcf8cd1a3985fa6a39065e
sigal1980 <aleks.sigaev@gmail.com>
2024-03-12 20:56:53 +0300

    Add base models
    Create package src.models.
    Create modules in src.models:
        base_price.py implements class BasePrice
        base_unit implements class BaseUnit
        base_category implements class BaseCategory
        base_quantity implements class BaseQuantity
        base_product implements class BaseProduct

---
+++ src/models/__init__.py
@@ -0,0 +1,7 @@
+from .base_price import BasePrice
+from .base_unit import BaseUnit
+from .base_quantity import BaseQuantity
+from .base_category import BaseCategory
+from .base_product import BaseProduct
+
+
